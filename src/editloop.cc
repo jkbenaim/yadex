8ff04ffb5f8b6822796e0c36ad02a6b12d8f6f22
Jason Benaim <jkbenaim@gmail.com>
2015-06-21 13:22:54 -0400

    Press F to add object

--- src/editloop.cc
+++ src/editloop.cc
@@ -304,7 +304,7 @@ e.mb_menu[MBM_FILE] = new Menu (NULL,
 
 e.mb_menu[MBM_EDIT] = new Menu (NULL,
    "~Copy object(s)",          'o',    0,
-   "~Add object",              YK_INS, 0,
+   "~Add object",              'f', 0,
    "~Delete object(s)",        YK_DEL, 0,
    "~Exchange object numbers", 24,     0,
    "~Preferences...",          YK_F5,  0,
@@ -2015,7 +2015,7 @@ cancel_save_as:
 	 }
 
       // [Ins]: insert a new object
-      else if (is.key == YK_INS || is.key == YK_INS + YK_SHIFT) /* 'Ins' */
+      else if (is.key == 'f' || is.key == 'f' + YK_SHIFT) /* 'Ins' */
 	 {
 	 SelPtr cur;
          int prev_obj_type = e.obj_type;
